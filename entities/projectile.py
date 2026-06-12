a80152447f22946859f1127ffc7271c41872bf01
James Gale <jtrevag@gmail.com>
2026-06-12 10:28:58 -0500

    feat: refactor Bullet to accept explicit radius/color/shape/speed params

--- entities/projectile.py
+++ entities/projectile.py
@@ -1,7 +1,7 @@
+import math
 import pygame
 from settings import (
     ARENA_LEFT, ARENA_TOP, ARENA_RIGHT, ARENA_BOTTOM,
-    PLAYER_DAMAGE, BULLET_SPEED, BULLET_RADIUS, BULLET_COLOR,
     BANDIT_PROJECTILE_SPEED, BANDIT_PROJECTILE_RADIUS,
     BANDIT_PROJECTILE_COLOR, BANDIT_PROJECTILE_DAMAGE,
 )
@@ -13,6 +13,7 @@ class Projectile(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(pos)
         self.vel = direction * speed
         self.damage = damage
+        self.radius = radius
         self.rect = pygame.Rect(0, 0, radius * 2, radius * 2)
         self.rect.center = (int(self.pos.x), int(self.pos.y))
 
@@ -25,11 +26,27 @@ class Projectile(pygame.sprite.Sprite):
 
 
 class Bullet(Projectile):
-    def __init__(self, pos, direction, damage=PLAYER_DAMAGE):
-        super().__init__(pos, direction, BULLET_SPEED, BULLET_RADIUS, damage)
+    def __init__(self, pos, direction, damage, radius, color, shape, speed):
+        super().__init__(pos, direction, speed, radius, damage)
+        self.color = color
+        self.shape = shape
 
     def draw(self, surface):
-        pygame.draw.circle(surface, BULLET_COLOR, self.rect.center, BULLET_RADIUS)
+        if self.shape == 'circle':
+            pygame.draw.circle(surface, self.color, self.rect.center, self.radius)
+        elif self.shape == 'rect':
+            cx, cy = self.rect.center
+            angle = math.atan2(self.vel.y, self.vel.x)
+            hl = self.radius * 2
+            hw = max(1, self.radius // 2)
+            cos_a, sin_a = math.cos(angle), math.sin(angle)
+            pts = [
+                (cx + cos_a * hl - sin_a * hw, cy + sin_a * hl + cos_a * hw),
+                (cx + cos_a * hl + sin_a * hw, cy + sin_a * hl - cos_a * hw),
+                (cx - cos_a * hl + sin_a * hw, cy - sin_a * hl - cos_a * hw),
+                (cx - cos_a * hl - sin_a * hw, cy - sin_a * hl + cos_a * hw),
+            ]
+            pygame.draw.polygon(surface, self.color, pts)
 
 
 class BanditProjectile(Projectile):
@@ -37,4 +54,4 @@ class BanditProjectile(Projectile):
         super().__init__(pos, direction, BANDIT_PROJECTILE_SPEED, BANDIT_PROJECTILE_RADIUS, BANDIT_PROJECTILE_DAMAGE)
 
     def draw(self, surface):
-        pygame.draw.circle(surface, BANDIT_PROJECTILE_COLOR, self.rect.center, BANDIT_PROJECTILE_RADIUS)
+        pygame.draw.circle(surface, BANDIT_PROJECTILE_COLOR, self.rect.center, self.radius)
